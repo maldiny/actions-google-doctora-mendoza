7c3e4d81ff300049ce91653c18771dd90b9a61f0
Rubén Maldonado Fernández <rmaldonadofernandez@gmail.com>
2018-07-19 08:40:39 +0200

    Primera versión sin backend

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,52 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { SlimLoadingBarModule } from 'ng2-slim-loading-bar';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { EnfermedadesComponent } from './components/enfermedades/enfermedades.component';
+import { PreguntasComponent } from './components/preguntas/preguntas.component';
+import { SintomasComponent } from './components/sintomas/sintomas.component';
+
+import { EnfermedadesService } from './components/services/enfermedades.service';
+import { SintomasService } from './components/services/sintomas.service';
+import { PreguntasService } from './components/services/preguntas.service';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: EnfermedadesComponent
+  },
+  {
+    path: 'preguntas',
+    component: PreguntasComponent
+  },
+  {
+    path: 'sintomas',
+    component: SintomasComponent
+  }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EnfermedadesComponent,
+    PreguntasComponent,
+    SintomasComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    SlimLoadingBarModule,
+    HttpClientModule,
+    RouterModule.forRoot(routes)
   ],
-  providers: [],
+  providers: [
+    EnfermedadesService,
+    SintomasService,
+    PreguntasService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
