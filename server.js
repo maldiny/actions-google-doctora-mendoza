899f9c61cb89af693072e7dae6ab8947cc6bf19e
Rubén Maldonado Fernández <rmaldonadofernandez@gmail.com>
2018-08-02 19:42:29 +0200

    Heroku db configuration

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const sintomasRoutes = require('./routes/sintomas.route');
 const enfermedadesRoutes = require('./routes/enfermedades.route');
 
 mongoose.Promise = global.Promise;
-mongoose.connect(config.DB, { useNewUrlParser: true }).then(
+mongoose.connect(process.env.MONGODB_URI || config.DB, { useNewUrlParser: true }).then(
   () => {console.log('Database is connected') },
   err => { console.log('Can not connect to the database'+ err)}
 );
